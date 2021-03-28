9a5ea937e4c99fd814b84a793e3b44417f65e59c
Abe Voelker <_@abevoelker.com>
2021-03-28 17:10:03 -0500

    Add append string input

--- lib/simple_form/tailwind/form_builder.rb
+++ lib/simple_form/tailwind/form_builder.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require "simple_form/form_builder"
+require "simple_form/tailwind/inputs/append_string_input"
 require "simple_form/tailwind/inputs/prepend_string_input"
 require "simple_form/tailwind/inputs/string_input"
 
@@ -8,6 +9,7 @@ module SimpleForm
     class FormBuilder < SimpleForm::FormBuilder
       map_type :string, :email, :search, :tel, :url, :uuid, :citext, to: SimpleForm::Tailwind::Inputs::StringInput
       map_type :prepend_string,                                      to: SimpleForm::Tailwind::Inputs::PrependStringInput
+      map_type :append_string,                                       to: SimpleForm::Tailwind::Inputs::AppendStringInput
 
       def error_notification(options = {})
         SimpleForm::Tailwind::ErrorNotification.new(self, options).render
---
+++ lib/simple_form/tailwind/inputs/append_string_input.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+require "simple_form/inputs/string_input"
+require "simple_form/tailwind/overwrite_class_with_error_or_valid_class"
+
+module SimpleForm
+  module Tailwind
+    module Inputs
+      class AppendStringInput < SimpleForm::Inputs::StringInput
+        include SimpleForm::Tailwind::OverwriteClassWithErrorOrValidClass
+
+        def input(*args, &blk)
+          input_html_options[:type] ||= "text"
+
+          super
+        end
+
+        def append(wrapper_options = nil)
+          template.content_tag(:span, options[:append], class: "inline-flex items-center px-3 rounded-r-md border border-l-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm")
+        end
+      end
+    end
+  end
+end
